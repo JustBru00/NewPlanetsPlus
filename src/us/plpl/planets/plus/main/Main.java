9c3a4cd0442379dbe2127f0ebea2310a69271fda
ComputerDude <playbaseball@gmail.com>
2017-01-09 20:25:13 -0500

    Added Stuff (I Think It's Done)

--- src/us/plpl/planets/plus/main/Main.java
+++ src/us/plpl/planets/plus/main/Main.java
@@ -2,8 +2,12 @@ package us.plpl.planets.plus.main;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import us.plpl.planets.plus.items.CustomItems;
 
 public class Main extends JavaPlugin {
 
@@ -14,6 +18,9 @@ public class Main extends JavaPlugin {
 		@SuppressWarnings("unused")
 		PluginManager pm = Bukkit.getServer().getPluginManager();
 
+		ShapedRecipe spaceHelmRecipe = new ShapedRecipe(CustomItems.getSpaceHelmet()).shape("III", "IGI", "GGG").setIngredient('I', Material.IRON_BLOCK).setIngredient('G', Material.GLASS);
+		getServer().addRecipe(spaceHelmRecipe);
+		
 	}
 
 	@Override
--- src/us/plpl/planets/plus/planets/Planet.java
+++ src/us/plpl/planets/plus/planets/Planet.java
@@ -1,10 +1,21 @@
 package us.plpl.planets.plus.planets;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import us.plpl.planets.plus.items.CustomItems;
 import us.plpl.planets.plus.main.Main;
 
 public class Planet implements Listener {
@@ -14,29 +25,105 @@ public class Planet implements Listener {
 	public Planet(Main plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void onPlayerSwitchWorlds(PlayerChangedWorldEvent e) {
-		if(!e.getPlayer().getWorld().getName().contains("TARDIS")) {
-			
-			String temp = e.getPlayer().getWorld().getName();
+
+		Player player = e.getPlayer();
+
+		// Changed World
+		if (!player.getWorld().getName().contains("TARDIS")) {
+
+			String temp = player.getWorld().getName();
 			char tempc = temp.charAt(0);
 			temp = temp.substring(1);
 			String temps = Character.toString(tempc);
 			temps = temps.toUpperCase();
 			temp = temps + temp;
-			
-		e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &3You have transported to " + temp));
+
+			player.sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &3You have transported to " + temp + " ("
+					+ plugin.getConfig().getString(e.getPlayer().getWorld().getName() + ".number") + ")"));
+			Bukkit.getConsoleSender().sendMessage(player.getName() + " went to planet: " + temp);
+			// Has Oxygen
+			if (plugin.getConfig().getBoolean("planets." + player.getWorld().getName() + ".canBreathe") == false
+					&& player.getInventory().getHelmet() != CustomItems.getSpaceHelmet()) {
+
+				player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 999999, 0));
+
+			} else {
+				player.removePotionEffect(PotionEffectType.POISON);
+			}
+
 		}
 	}
-	
+
 	@EventHandler
-	public void onBlockBreak(BlockBreakEvent e) {
+	public void onPlayerDrinkMilk(PlayerInteractEvent e) {
+		if (!e.getPlayer().getWorld().getName().contains("TARDIS")) {
+			if (plugin.getConfig().getBoolean("planets." + e.getPlayer().getWorld().getName() + ".canBreathe") == false
+					&& e.getPlayer().getInventory().getItemInMainHand().getType() == Material.MILK_BUCKET) {
 
-		if (plugin.getConfig().getBoolean("planets." + e.getPlayer().getWorld().getName() + ".Can-Modify") == false) {
-			e.setCancelled(true);
-		} 
-
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &7&oWear a space helmet to have oxygen on this planet!"));
+			}
+		}
 	}
 
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent e) {
+		if (!e.getPlayer().getWorld().getName().contains("TARDIS")) {
+			if (plugin.getConfig()
+					.getBoolean("planets." + e.getPlayer().getWorld().getName() + ".canModify") == false) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &7&oYou can't edit this planet!"));
+			}
+		}
+	}
+
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent e) {
+		if (!e.getPlayer().getWorld().getName().contains("TARDIS")) {
+			if (plugin.getConfig()
+					.getBoolean("planets." + e.getPlayer().getWorld().getName() + ".canModify") == false) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &7&oYou can't edit this planet!"));
+			}
+		}
+	}
+
+	@EventHandler
+	public void onCommandRun(PlayerCommandPreprocessEvent e) {
+		if (!e.getPlayer().getWorld().getName().contains("TARDIS")) {
+
+			//Can TPA from Earth (world)
+			if(plugin.getConfig().getBoolean(e.getPlayer().getWorld() + ".canTpaTo") && e.getMessage().contains("tpaccept")) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &7&oYou can't tp to this planet!"));
+			}
+			
+			// Can Claim
+			if (plugin.getConfig().getBoolean("planets." + e.getPlayer().getWorld().getName() + ".canClaim") == false) {
+				if (e.getMessage().contains("town add") || e.getMessage().contains("t add")
+						|| e.getMessage().contains("t claim") || e.getMessage().contains("town claim")) {
+					e.setCancelled(true);
+					e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &7&oYou can't claim on this planet!"));
+				}
+			}
+
+			// Can Sethome
+			if (plugin.getConfig().getBoolean(e.getPlayer().getWorld().getName() + "setHome") == false
+					&& e.getMessage().contains("sethome")) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(Main.color("&7&l[&c&lPlus+&7&l] &7&oYou can't set home on this planet!"));
+			}
+		}
+	}
+
+	@EventHandler
+	public void onMobSpawn(EntitySpawnEvent e) {
+		if (!e.getEntity().getWorld().getName().contains("TARDIS") && e.getEntity().getType() != EntityType.PLAYER
+				&& plugin.getConfig().getBoolean(e.getEntity().getWorld().getName() + ".supportsLife") == false) {
+			e.setCancelled(true);
+		}
+	}
 }
